7f51d43b7d8ba9cdba8e90b265d2c66039da534b
Monisha-RK10 <monisha.rk2415@gmail.com>
2025-06-12 07:38:25 +0200

    Update yolov8_detect.py

--- scripts/yolov8_detect.py
+++ scripts/yolov8_detect.py
@@ -10,17 +10,17 @@ from PIL import Image
 import cv2
 
 def compute_iou(boxA, boxB):
-    # box: [x1, y1, x2, y2]
+    # box: [x1, y1, x2, y2] consider max for 0, 1 and min for 2, 3
     xA = max(boxA[0], boxB[0])
     yA = max(boxA[1], boxB[1])
     xB = min(boxA[2], boxB[2])
     yB = min(boxA[3], boxB[3])
 
-    interArea = max(0, xB - xA) * max(0, yB - yA)
+    interArea = max(0, xB - xA) * max(0, yB - yA)                  
     if interArea == 0:
         return 0.0
 
-    boxAArea = (boxA[2] - boxA[0]) * (boxA[3] - boxA[1])
+    boxAArea = (boxA[2] - boxA[0]) * (boxA[3] - boxA[1])                                           # x2-x1 * y2-y1
     boxBArea = (boxB[2] - boxB[0]) * (boxB[3] - boxB[1])
     iou = interArea / float(boxAArea + boxBArea - interArea)
     return iou
@@ -31,7 +31,7 @@ model = YOLO("yolov8s.pt")
 input_folder = "/content/drive/MyDrive/kitti_tracking/data_tracking_image_2/training/image_02/0000"
 output_det_file = "/content/filtered_0000.txt"
 
-wanted_classes = {'person': 0, 'bicycle': 1, 'car': 2}
+wanted_classes = {'person': 0, 'bicycle': 1, 'car': 2}                                             # as per COCO labels
 id_to_name = {v: k for k, v in wanted_classes.items()}
 
 results_lines = []
@@ -68,12 +68,12 @@ for filename in sorted(os.listdir(input_folder)):
         for i, pi in enumerate(persons):
             if i not in matched_person_indices:
                 x1, y1, x2, y2, conf = pi
-                results_lines.append(f"{frame_id},-1,{x1:.2f},{y1:.2f},{x2:.2f},{y2:.2f},{conf:.2f},0\n")  # 0 = person
+                results_lines.append(f"{frame_id},-1,{x1:.2f},{y1:.2f},{x2:.2f},{y2:.2f},{conf:.2f},0\n")  # 0 = person, frameid, trackid, xxyy, conf
 
         # Save cars
         for ci in cars:
             x1, y1, x2, y2, conf = ci
-            results_lines.append(f"{frame_id},-1,{x1:.2f},{y1:.2f},{x2:.2f},{y2:.2f},{conf:.2f},2\n")  # 2 = car
+            results_lines.append(f"{frame_id},-1,{x1:.2f},{y1:.2f},{x2:.2f},{y2:.2f},{conf:.2f},2\n")      # 2 = car
 
         frame_id += 1
 
