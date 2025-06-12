6b5b8caeb38e356a643719f4b767531efb06e5f1
Monisha-RK10 <monisha.rk2415@gmail.com>
2025-06-12 08:42:18 +0200

    Update bytetrack_tracker.py

--- scripts/bytetrack_tracker.py
+++ scripts/bytetrack_tracker.py
@@ -47,12 +47,12 @@ with open(det_file, "r") as f:
 # Solution: Match 'track_box' from BYTETrack to a detection box from YOLO with the highest IoU overlap and grabs its cls_id.
 
 def compute_iou(box1, box2):
-    # tl(max): x1[0], y1[1], br(min): x2[2], y2[3]
+    # x1 y1 x2 y2 i.e., 0, 1, 2, 3. max for x1, y1 and min for x2, y2 between box1 and box2
     xi1, yi1 = max(box1[0], box2[0]), max(box1[1], box2[1])         # top-left of intersection (max)
     xi2, yi2 = min(box1[2], box2[2]), min(box1[3], box2[3])         # bottom-right of intersection (min)
     inter_area = max(0, xi2 - xi1) * max(0, yi2 - yi1)              # width × height
 
-    box1_area = (box1[2] - box1[0]) * (box1[3] - box1[1])
+    box1_area = (box1[2] - box1[0]) * (box1[3] - box1[1])           # x2-x1 * y2-y1
     box2_area = (box2[2] - box2[0]) * (box2[3] - box2[1])
     union_area = box1_area + box2_area - inter_area
 
