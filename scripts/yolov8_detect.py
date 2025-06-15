a08540c3d97377b13c7e1e9e97cd52221b48cfea
Monisha-RK10 <monisha.rk2415@gmail.com>
2025-06-15 04:47:21 +0200

    Update yolov8_detect.py

--- scripts/yolov8_detect.py
+++ scripts/yolov8_detect.py
@@ -20,7 +20,7 @@ def compute_iou(boxA, boxB):
     if interArea == 0:
         return 0.0
 
-    boxAArea = (boxA[2] - boxA[0]) * (boxA[3] - boxA[1])                                           # x2-x1 * y2-y1
+    boxAArea = (boxA[2] - boxA[0]) * (boxA[3] - boxA[1])                                                # x2-x1 * y2-y1
     boxBArea = (boxB[2] - boxB[0]) * (boxB[3] - boxB[1])
     iou = interArea / float(boxAArea + boxBArea - interArea)
     return iou
@@ -31,7 +31,7 @@ model = YOLO("yolov8s.pt")
 input_folder = "/content/drive/MyDrive/kitti_tracking/data_tracking_image_2/training/image_02/0000"
 output_det_file = "/content/filtered_0000.txt"
 
-wanted_classes = {'person': 0, 'bicycle': 1, 'car': 2}                                             # as per COCO labels
+wanted_classes = {'person': 0, 'bicycle': 1, 'car': 2}                                                  # As per COCO labels
 id_to_name = {v: k for k, v in wanted_classes.items()}
 
 results_lines = []
@@ -59,7 +59,7 @@ for filename in sorted(os.listdir(input_folder)):
 
         matched_person_indices = set()
         for bi in bicycles:
-            for i, pi in enumerate(persons):                                                              # Example: persons = [p1, p2, p3]  # 3 persons, bicycles = [b1], If b1 overlaps with p2, then: matched_person_indices = {1}
+            for i, pi in enumerate(persons):                                                               # Example: persons = [p1, p2, p3]  # 3 persons, bicycles = [b1], If b1 overlaps with p2, then: matched_person_indices = {1}
                 iou = compute_iou(bi[:4], pi[:4])
                 if iou > 0.4:  # Cyclist detected
                     matched_person_indices.add(i)
