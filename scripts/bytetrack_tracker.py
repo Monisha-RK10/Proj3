e9183662203d9185e946d95e32209089e48a2af9
Monisha-RK10 <monisha.rk2415@gmail.com>
2025-06-15 05:00:05 +0200

    Update bytetrack_tracker.py

--- scripts/bytetrack_tracker.py
+++ scripts/bytetrack_tracker.py
@@ -79,11 +79,11 @@ for frame_id in range(len(image_files)):
 
     dets = frame_detections.get(frame_id, [])                                   # If no detecions, return empty
     if len(dets) > 0:
-        dets_np = np.array([[*d[:4], d[4]] for d in dets], dtype=np.float32)    # cls_id is removed, ** is used for dictionaries, * is used for lists or tuples
+        dets_np = np.array([[*d[:4], d[4]] for d in dets], dtype=np.float32)    # cls_id is removed. ** is used for dictionaries. * is used for lists or tuples
     else:
         dets_np = np.empty((0, 5), dtype=np.float32)                            # cls_id is removed 
 
-    tracks = tracker.update(dets_np, img_info=(h, w), img_size=(h, w))          # (original_h, original_w). (resized_h, resized_w)
+    tracks = tracker.update(dets_np, img_info=(h, w), img_size=(h, w))          # (original_h, original_w), (resized_h, resized_w)
 
     for t in tracks:
         x1, y1, x2, y2 = t.tlbr
@@ -94,7 +94,7 @@ for frame_id in range(len(image_files)):
         best_iou = 0
         best_cls_id = -1
         for det in dets:                                                        # dets = YOLO detections for this frame, cls_id is present
-            iou = compute_iou(track_box, det[:4])                               # matching tracked box (no class info) with detection box (has class info)
+            iou = compute_iou(track_box, det[:4])                               # Matching tracked box (no class info) with detection box (has class info)
             if iou > best_iou:
                 best_iou = iou
                 best_cls_id = int(det[5])                                       # get class ID of best-matching YOLO box
